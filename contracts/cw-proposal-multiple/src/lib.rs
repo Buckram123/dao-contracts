4ae6ea97539396b26f243480116b51fb0876cc18
Janita Chalam <janitachalam@gmail.com>
2022-02-16 11:17:28 -0800

    introduce multiple choice proposal as separate contract

---
+++ contracts/cw-proposal-multiple/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod constants;
+pub mod contract;
+mod error;
+pub mod msg;
+pub mod proposal;
+pub mod query;
+pub mod state;
+
+pub mod voting_strategy;
+pub use crate::error::ContractError;
+
+#[cfg(test)]
+mod tests;
